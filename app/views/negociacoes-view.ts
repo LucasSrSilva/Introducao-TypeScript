7636fd0828e98d7e7bd3af919f830ddbba2902a8
LucasSrSilva <lucas.srs.2022@gmail.com>
2024-02-16 22:46:06 -0300

    160224 curso typescript parte 2

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,14 +1,10 @@
 import { Negociacoes } from '../models/negociacoes.js';
+import { View } from './view.js';
 
-export class NegociacoesView {
+export class NegociacoesView extends View<Negociacoes>{
 
-    private elemento: HTMLElement;
 
-    constructor(seletor: string) {
-        this.elemento = document.querySelector(seletor);
-    }
-
-    template(model: Negociacoes): string {
+    protected template(model: Negociacoes): string {
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -22,15 +18,9 @@ export class NegociacoesView {
                 ${model.lista().map(negociacao => {
                     return `
                         <tr>
-                            <td>${new Intl.DateTimeFormat()
-                                .format(negociacao.data)}
-                            </td>
-                            <td>
-                                ${negociacao.quantidade}
-                            </td>
-                            <td>
-                                ${negociacao.valor}
-                            </td>
+                            <td>${this.formatar(negociacao.data)}</td>
+                            <td>${negociacao.quantidade}</td>
+                            <td>${negociacao.valor}</td>
                         </tr>
                     `;
                 }).join('')}
@@ -39,9 +29,7 @@ export class NegociacoesView {
         `;
     }
 
-    update(model: Negociacoes): void {
-        const template = this.template(model);
-        console.log(template);
-        this.elemento.innerHTML = template;
+    private formatar(data: Date){
+        return new Intl.DateTimeFormat().format(data)
     }
 }
